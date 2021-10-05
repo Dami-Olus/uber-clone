339ed9850f7a00d8dfd246f450734052a02a2a88
Damola Olusakin <olusakinadedamola@gmail.com>
2021-10-05 09:08:27 +0100

    changed reducer to reducers in navslice to fix error

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from "../slices/navSlice";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
      <View style={tw`p-5`}>
@@ -16,6 +21,39 @@ const HomeScreen = ({navigation}) => {
         }}
        />
      </View>
+
+     <GooglePlacesAutocomplete
+     placeholder="Where from"
+     styles={{
+       container: {
+         flex: 0,
+       },
+       textInput: {
+         fontSize: 18,
+       },
+     }}
+     onPress={(data, details = null) => {
+        dispatch(setOrigin({
+          location: details.geometry.location,
+          description: data.description,
+        }));
+
+        dispatch(setDestination(null))
+        // console.log(details.geometry.location)
+        
+        
+     }}
+     enablePoweredByContainer={false}
+
+     query={{
+       key: GOOGLE_MAPS_APIKEY,
+       language: 'en',
+     }}
+      nearbyPlacesAPI="GooglePlacesSearch"
+      debounce={400}
+      fetchDetails={true}
+     />
+
      <NavOptions />
     </SafeAreaView>
   )
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -9,18 +9,20 @@ const initialState = {
 export const navSlice = createSlice({
   name: 'nav',
   initialState,
-  reducer: {
+  reducers: {
     setOrigin: (state, action) => {
       state.origin = action.payload;
     },
     setDestination: (state, action) => {
       state.destination = action.payload;
     },
-    setOrigin: (state, action) => {
+    setTravelTimeInformation: (state, action) => {
       state.travelTimeInformation = action.payload;
     }
   }
 });
+
+
 
 export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
 
