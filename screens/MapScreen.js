f2152a4bf6198177b166a7b5ff8a65303914c5ac
Damola Olusakin <olusakinadedamola@gmail.com>
2021-10-06 14:34:22 +0100

    added ride options screen and navigation card

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import tw from "tailwind-react-native-classnames"
 import Map from '../components/Map'
+import NavigationCard from '../components/NavigationCard'
 import MapView from 'react-native-maps'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import RideOptionsCard from '../components/RideOptionsCard'
+
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator()
+  
   return (
     <View>
       <Text>Here is the Map stuff...</Text>
@@ -12,7 +18,24 @@ const MapScreen = () => {
         <Map />
       </View>
 
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen 
+          name="NavigationCard"
+          component={NavigationCard}
+          options={{
+            headerShown: false,
+          }}
+          />
+          <Stack.Screen 
+          name="RideOptionsCard"
+          component={RideOptionsCard}
+          options={{
+            headerShown: false,
+          }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   )
 }
