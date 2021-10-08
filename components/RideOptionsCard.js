ff7a13850e9682dd1784be06881a0df40c9ad32a
Damola Olusakin <olusakinadedamola@gmail.com>
2021-10-09 00:08:03 +0100

    added the choose ride button to ride options screen

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import {
   FlatList,
   Image,
@@ -35,6 +35,7 @@ const data = [
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
@@ -52,11 +53,14 @@ const RideOptionsCard = () => {
         keyExtractor={(item) => item.id}
         renderItem={({ item: { id, title, multiplier, image }, item }) => (
           <TouchableOpacity
-            style={tw`flex-row justify-between items-center px-10`}
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
           >
             <Image
               style={{
-                width: 110,
+                width: 100,
                 height: 100,
                 resizeMode: "contain",
               }}
@@ -70,6 +74,13 @@ const RideOptionsCard = () => {
           </TouchableOpacity>
         )}
       />
+      <View>
+        <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}>
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
