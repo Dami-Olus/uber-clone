3c35e11c5ff5e32d05e5eab0341878325a7e9a4b
Damola Olusakin <olusakinadedamola@gmail.com>
2021-10-09 07:35:07 +0100

    added travel time and price information

--- components/Map.js
+++ components/Map.js
@@ -1,51 +1,67 @@
-import React, { useEffect, useRef } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import MapView, { Marker } from "react-native-maps"
-import MapViewDirections from 'react-native-maps-directions'
-import { useSelector } from 'react-redux'
-import tw from "tailwind-react-native-classnames"
-import { selectDestination, selectOrigin } from '../slices/navSlice'
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+import { useDispatch, useSelector } from "react-redux";
+import tw from "tailwind-react-native-classnames";
+import {
+  selectDestination,
+  selectOrigin,
+  setTravelTimeInformation,
+} from "../slices/navSlice";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 
-
 const Map = () => {
-  const origin = useSelector(selectOrigin)
-  const destination = useSelector(selectDestination)
-  const mapRef = useRef(null)
-  console.log(origin)
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!origin || !destination) return;
 
-    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
       edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
     });
-  }, [origin, destination])
+  }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    const getTravelTime = async () => {
+      fetch(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+        });
+    };
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_APIKEY]);
 
   return (
     <MapView
-    ref={mapRef}
-    style={tw`flex-1`}
-    maptype="mutedStandard"
-    initialRegion={{
-      latitude: origin.location.lat,
-      longitude: origin.location.lng,
-      latitudeDelta: 0.005,
-      longitudeDelta: 0.005,
-    }}
-  >
-
-    {origin && destination && (
-      <MapViewDirections 
-      origin={origin.description}
-      destination={destination.description}
-      apikey={GOOGLE_MAPS_APIKEY}
-      strokeWidth={3}
-      strokeColor="green"
-      />
-    )}
+      ref={mapRef}
+      style={tw`flex-1`}
+      maptype="mutedStandard"
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="green"
+        />
+      )}
       {origin?.location && (
-        <Marker 
+        <Marker
           coordinate={{
             latitude: origin.location.lat,
             longitude: origin.location.lng,
@@ -56,8 +72,8 @@ const Map = () => {
         />
       )}
 
-{destination?.location && (
-        <Marker 
+      {destination?.location && (
+        <Marker
           coordinate={{
             latitude: destination.location.lat,
             longitude: destination.location.lng,
@@ -67,11 +83,10 @@ const Map = () => {
           identifier="destination"
         />
       )}
-
     </MapView>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -10,7 +10,9 @@ import {
   View,
 } from "react-native";
 import { Icon } from "react-native-elements";
+import { useSelector } from "react-redux";
 import tw from "tailwind-react-native-classnames";
+import { selectTravelTimeInformation } from "../slices/navSlice";
 
 const data = [
   {
@@ -33,9 +35,12 @@ const data = [
   },
 ];
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
@@ -45,7 +50,9 @@ const RideOptionsCard = () => {
         >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center py-5 text-xl`}>
+          Select a Ride - {travelTimeInformation?.distance.text}{" "}
+        </Text>
       </View>
 
       <FlatList
@@ -60,22 +67,35 @@ const RideOptionsCard = () => {
           >
             <Image
               style={{
-                width: 100,
-                height: 100,
+                width: 90,
+                height: 90,
                 resizeMode: "contain",
               }}
               source={{ uri: image }}
             />
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travel time...</Text>
+              <Text>{travelTimeInformation?.duration.text}</Text>
             </View>
-            <Text style={tw`text-xl`}>$99</Text>
+            <Text style={tw`text-xl`}>
+              {new Intl.NumberFormat("en-gb", {
+                style: "currency",
+                currency: "NGN",
+              }).format(
+                (travelTimeInformation?.duration.value *
+                  SURGE_CHARGE_RATE *
+                  multiplier) /
+                  100
+              )}
+            </Text>
           </TouchableOpacity>
         )}
       />
       <View>
-        <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+        >
           <Text style={tw`text-center text-white text-xl`}>
             Choose {selected?.title}
           </Text>
